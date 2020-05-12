2bb6530084d2454293a25c6688f2011613ba016f
elijah-dev <elijah.khrebtov@gmail.com>
2020-05-12 19:55:44 +0300

    minimist fix and a small change

--- client/src/components/board/Board.js
+++ client/src/components/board/Board.js
@@ -5,39 +5,49 @@ import { useSelector } from 'react-redux';
 import AuthContainer from './AuthContainer';
 
 const Board = () => {
-  const isAuthorized = useSelector(state => state.currentUser.isAuthorized);
+  const isAuthorized = useSelector((state) => state.currentUser.isAuthorized);
+  const project = useSelector((state) => state.currentProject.data);
 
   if (isAuthorized) {
+    if (project.name) {
+      return (
+        <Container className='board'>
+          <Row className='title-row'>
+            <Col>
+              <h4>
+                <Badge color='danger'>Open</Badge>
+              </h4>
+            </Col>
+            <Col>
+              <h4>
+                <Badge color='warning'>Assigned</Badge>
+              </h4>
+            </Col>
+            <Col>
+              <h4>
+                <Badge color='info'>In Progress</Badge>
+              </h4>
+            </Col>
+            <Col>
+              <h4>
+                <Badge color='success'>Resolved</Badge>
+              </h4>
+            </Col>
+          </Row>
+          <Row className='issue-row'>
+            <IssueColumn status='open' />
+            <IssueColumn status='assigned' />
+            <IssueColumn status='in progress' />
+            <IssueColumn status='resolved' />
+          </Row>
+        </Container>
+      );
+    }
     return (
-      <Container className='board'>
-        <Row className='title-row'>
-          <Col>
-            <h4>
-              <Badge color='danger'>Open</Badge>
-            </h4>
-          </Col>
-          <Col>
-            <h4>
-              <Badge color='warning'>Assigned</Badge>
-            </h4>
-          </Col>
-          <Col>
-            <h4>
-              <Badge color='info'>In Progress</Badge>
-            </h4>
-          </Col>
-          <Col>
-            <h4>
-              <Badge color='success'>Resolved</Badge>
-            </h4>
-          </Col>
-        </Row>
-        <Row className='issue-row'>
-          <IssueColumn status='open' />
-          <IssueColumn status='assigned' />
-          <IssueColumn status='in progress' />
-          <IssueColumn status='resolved' />
-        </Row>
+      <Container
+        className='d-flex align-items-center justify-content-center'
+        style={{ height: '60%' }}>
+        <h1 style={{ opacity: 0.6 }}>No project chosen</h1>
       </Container>
     );
   }
